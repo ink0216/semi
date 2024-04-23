f820e77fecd732251e39f6804a5d794c1ea25d5a
NTH <tang0929@naver.com>
2024-04-23 17:51:28 +0900

    123

--- travelBoard/src/main/java/edu/kh/travel/member/model/mapper/MemberMapper.java
+++ travelBoard/src/main/java/edu/kh/travel/member/model/mapper/MemberMapper.java
@@ -2,16 +2,11 @@ package edu.kh.travel.member.model.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
-<<<<<<< HEAD
 import edu.kh.travel.member.model.dto.Member;
 
 @Mapper
 public interface MemberMapper {
 
 	public Member login(String memberEmail);
-=======
-@Mapper
-public interface MemberMapper {
->>>>>>> 3e8035eb9d1a32e461dbb7b910db097412cd45de
 
 }
