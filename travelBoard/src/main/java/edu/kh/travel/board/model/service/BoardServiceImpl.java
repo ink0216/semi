4483f2ca846331fa9a8a39b4d4791b9d41d4c7e9
InkyungLee <yklee980216@naver.com>
2024-04-23 14:12:15 +0900

    no message

--- travelBoard/src/main/java/edu/kh/travel/board/model/service/BoardServiceImpl.java
+++ travelBoard/src/main/java/edu/kh/travel/board/model/service/BoardServiceImpl.java
@@ -1,5 +1,15 @@
 package edu.kh.travel.board.model.service;
 
-public class BoardServiceImpl {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import edu.kh.travel.member.model.mapper.MemberMapper;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService{
+	private final MemberMapper mapper;
+	
 }
